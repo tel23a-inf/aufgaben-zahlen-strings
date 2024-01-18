edb1f0f9ca465055f25e056dbffdada93c31c319
Reiner Hüchting <dev@reinerhuechting.de>
2024-01-18 19:08:55 +0100

    Remove solutions.

--- roman/roman.go
+++ roman/roman.go
@@ -1,13 +1,11 @@
 package roman
 
-import "strings"
-
 // NToI erwartet eine Zahl und liefert die entsprechende Anzahl an I als String.
 func NToI(n int) string {
 	/* Hinweis:
 	Verwenden Sie die Funktion strings.Repeat.
 	*/
-	return strings.Repeat("I", n)
+	return ""
 }
 
 // NToX erwartet eine Zahl und liefert die entsprechende Anzahl an X als String.
@@ -15,7 +13,7 @@ func NToX(n int) string {
 	/* Hinweis:
 	Verwenden Sie die Funktion strings.Repeat.
 	*/
-	return strings.Repeat("X", n)
+	return ""
 }
 
 // RomanBelow10 erwartet eine Zahl 1 <= n <= 10 und liefert die römische Schreibweise für n als String.
@@ -24,21 +22,6 @@ func RomanBelow10(n int) string {
 	Unterscheiden Sie die Fälle n == 0, n <= 3, n <= 5, n <= 8 und n <= 10.
 	Für jeden dieser Fälle lässt sich mittels der Funktion NToI() ein Teil des Ergebnisses berechnen.
 	*/
-	if n == 0 {
-		return ""
-	}
-	if n <= 3 {
-		return NToI(n)
-	}
-	if n <= 5 {
-		return NToI(5-n) + "V"
-	}
-	if n <= 8 {
-		return "V" + NToI(n-5)
-	}
-	if n <= 10 {
-		return NToI(10-n) + "X"
-	}
 	return ""
 }
 
@@ -49,17 +32,5 @@ func RomanBelow100(n int) string {
 	Für jeden dieser Fälle lässt sich mittels der Funktionen NToX() und RomanBelow10()
 	ein Teil des Ergebnisses berechnen.
 	*/
-	if n <= 39 {
-		return NToX(n/10) + RomanBelow10(n%10)
-	}
-	if n <= 59 {
-		return NToX((59-n)/10) + "L" + RomanBelow10(n%10)
-	}
-	if n <= 89 {
-		return "L" + NToX((n-50)/10) + RomanBelow10(n%10)
-	}
-	if n <= 109 {
-		return NToX((109-n)/10) + "C" + RomanBelow10(n%10)
-	}
 	return ""
 }
